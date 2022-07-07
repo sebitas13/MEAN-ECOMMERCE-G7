c21e8574c4368479d4c4dcb3d52d1dd2128dbfcc
sebas <jesus.huamanculi1@unmsm.edu.pe>
2022-07-07 17:31:33 -0500

    galeria producto

--- backend/controllers/productoController.js
+++ backend/controllers/productoController.js
@@ -243,6 +243,52 @@ const actualizar_producto_variedades_admin = async function(req, res){
     }
 }
 
+const agregar_imagen_galeria_admin = async function(req, res){
+    if(req.user){
+        if(req.user.role == 'admin'){
+
+            let id = req.params['id'];
+            let data = req.body;
+
+            var img_path = req.files.imagen.path;
+            var name = img_path.split('\\');
+            var imagen_name = name[2];
+
+            let reg = await Producto.findByIdAndUpdate({_id:id},{$push:{galeria:{imagen:imagen_name,_id:data._id}}});
+           
+           
+            res.status(200).send({data:reg});
+
+        }else{
+            res.status(200).send({mensaje:'Error server hash',data:undefined});
+        }
+    }else{
+        res.status(200).send({mensaje:'Error server hash',data:undefined});
+    }
+}
+
+
+const eliminar_imagen_galeria_admin = async function(req, res){
+    if(req.user){
+        if(req.user.role == 'admin'){
+
+            let id = req.params['id'];
+            let data = req.body;
+
+            
+
+            let reg = await Producto.findByIdAndUpdate({_id:id},{$pull:{galeria:{_id:data._id}}});
+           
+           
+            res.status(200).send({data:reg});
+
+        }else{
+            res.status(200).send({mensaje:'Error server hash',data:undefined});
+        }
+    }else{
+        res.status(200).send({mensaje:'Error server hash',data:undefined});
+    }
+}
 module.exports = {
     registro_producto_admin,
     listar_productos_admin,
@@ -253,4 +299,6 @@ module.exports = {
     listar_inventario_producto_admin,
     eliminar_inventario_producto_admin,
     actualizar_producto_variedades_admin,
+    agregar_imagen_galeria_admin,
+    eliminar_imagen_galeria_admin,
 }
--- backend/routes/producto.js
+++ backend/routes/producto.js
@@ -16,10 +16,15 @@ api.get('/obtener_producto_admin/:id',auth.auth,productoController.obtener_produ
 api.put('/actualizar_producto_admin/:id',[auth.auth,path],productoController.actualizar_producto_admin);
 api.delete('/eliminar_producto_admin/:id',auth.auth,productoController.eliminar_producto_admin);
 api.put('/actualizar_producto_variedades_admin/:id',auth.auth,productoController.actualizar_producto_variedades_admin);
+api.put('/agregar_imagen_galeria_admin/:id',[auth.auth,path],productoController.agregar_imagen_galeria_admin);
+api.put('/eliminar_imagen_galeria_admin/:id',auth.auth,productoController.eliminar_imagen_galeria_admin);
 
 //inventario
 api.get('/listar_inventario_producto_admin/:id',auth.auth,productoController.listar_inventario_producto_admin);
 api.delete('/eliminar_inventario_producto_admin/:id',auth.auth, productoController.eliminar_inventario_producto_admin);
+//api.post('/registro_inventario_producto_admin',auth.auth,productoController.registro_producto_admin);
 
+//public
+//api.get('listar_productos_publico/:filtro?',productoController.eliminar_productos_publico);
 
 module.exports= api;
