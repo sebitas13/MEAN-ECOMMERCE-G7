9a11a6b49932bb54c0ae448804dce99eceea74f6
sebas <jesus.huamanculi1@unmsm.edu.pe>
2022-07-23 20:31:35 -0500

    galeria dinamica ok

--- tienda/src/app/components/productos/show-producto/show-producto.component.ts
+++ tienda/src/app/components/productos/show-producto/show-producto.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { GLOBAL } from 'src/app/services/GLOBAL';
+import { GuestService } from 'src/app/services/guest.service';
 declare var tns;
 declare var lightGallery;
 
@@ -9,23 +12,49 @@ declare var lightGallery;
 })
 export class ShowProductoComponent implements OnInit {
 
-  constructor() { }
+  public slug;
+  public producto : any = {};
+  public url;
+  constructor(
+    private _route : ActivatedRoute,
+    private _guestService : GuestService
+  ) { 
+    this.url = GLOBAL.url;
+    this._route.params.subscribe(
+      params=>{
+        this.slug = params['slug'];
+        
+        
+        this._guestService.obtener_productos_slug_publico(this.slug).subscribe(
+          response=>{
+            this.producto = response.data;
+            
+          }
+        )
+      }
+    )
+  }
 
   ngOnInit(): void {
+
+    setTimeout(()=>{
+      tns({
+        container: '.cs-carousel-inner',
+        controlsText: ['<i class="cxi-arrow-left"></i>', '<i class="cxi-arrow-right"></i>'],
+        navPosition: "top",
+        controlsPosition: "top",
+        mouseDrag: !0,
+        speed: 600,
+        autoplayHoverPause: !0,
+        autoplayButtonOutput: !1,
+        navContainer: "#cs-thumbnails",
+        navAsThumbnails: true,
+        gutter: 15,
+      });
+    },500)
+
     //inicializar slider
-    tns({
-      container: '.cs-carousel-inner',
-      controlsText: ['<i class="cxi-arrow-left"></i>', '<i class="cxi-arrow-right"></i>'],
-      navPosition: "top",
-      controlsPosition: "top",
-      mouseDrag: !0,
-      speed: 600,
-      autoplayHoverPause: !0,
-      autoplayButtonOutput: !1,
-      navContainer: "#cs-thumbnails",
-      navAsThumbnails: true,
-      gutter: 15,
-    });
+   
 
     var e = document.querySelectorAll(".cs-gallery");
     if (e.length){
@@ -33,6 +62,41 @@ export class ShowProductoComponent implements OnInit {
         lightGallery(e[t], { selector: ".cs-gallery-item", download: !1, videojs: !0, youtubePlayerParams: { modestbranding: 1, showinfo: 0, rel: 0 }, vimeoPlayerParams: { byline: 0, portrait: 0 } });
       }
     }
+
+    
+          tns({
+            container: '.cs-carousel-inner-two',
+            controlsText: ['<i class="cxi-arrow-left"></i>', '<i class="cxi-arrow-right"></i>'],
+            navPosition: "top",
+            controlsPosition: "top",
+            mouseDrag: !0,
+            speed: 600,
+            autoplayHoverPause: !0,
+            autoplayButtonOutput: !1,
+            nav: false,
+            controlsContainer: "#custom-controls-related",
+            responsive: {
+              0: {
+                items: 1,
+                gutter: 20
+              },
+              480: {
+                items: 2,
+                gutter: 24
+              },
+              700: {
+                items: 3,
+                gutter: 24
+              },
+              1100: {
+                items: 4,
+                gutter: 30
+              }
+            }
+          });
+
+
   }
+
 
 }
---
+++ tienda/src/app/services/guest.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { GLOBAL } from './GLOBAL';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { JwtHelperService } from "@auth0/angular-jwt";
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GuestService {
+
+  public url;
+
+  constructor(private _http: HttpClient) {
+    this.url = GLOBAL.url;
+  }
+
+  obtener_productos_slug_publico(slug):Observable<any>{
+    let headers = new HttpHeaders().set('Content-Type','application/json');
+    return this._http.get(this.url+'obtener_productos_slug_publico/'+slug,{headers:headers});
+   }
+}
